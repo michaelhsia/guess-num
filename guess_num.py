6db22649acd47c0344e6855ae3c2fc0920ac4050
Michael Hsia <michaelhsia8716@gmail.com>
2021-10-02 09:24:16 +0800

    basic version

---
+++ guess_num.py
@@ -0,0 +1,12 @@
+import random
+r = random.randint(1, 100)
+while True:
+    num = input('請輸入數字: ')
+    num = int(num)
+    if num == r:
+        print('終於猜對了')
+        break
+    elif num > r:
+        print('比答案小喔')
+    elif num < r:
+        print('比答案大喔')
